34e6441d3951f404eb5670410a0a6260d797f082
Richard Shi <richardshi@mac-book-air.local>
2015-12-29 14:39:06 -0500

    Initial Commit

---
+++ BarcodeScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarcodeScanner
+//
+//  Created by Richard Shi on 12/29/15.
+//  Copyright © 2015 Richard Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
